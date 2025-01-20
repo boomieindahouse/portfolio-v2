46262464597ad0d6e7b3437296c493475fef3bb7
boomieindahouse <prawitsanat@hotmail.com>
2025-01-20 20:17:14 +0700

    edit h, w in showcase

--- src/components/home/Projects.js
+++ src/components/home/Projects.js
@@ -2,49 +2,51 @@ const PortfolioSection = () => {
     const projects = [
         {
             title: "Meeting Creative Company",
-            image: "https://i.postimg.cc/SR8VVws5/1.png",
+            image: "https://i.postimg.cc/yxg3c3Py/meetingweb.jpg",
         },
         {
             title: "Movie Recommendation",
-            image: "https://i.postimg.cc/MTgd7WWV/4.png",
+            image: "https://i.postimg.cc/RZQ67Bcy/movierec.jpg",
         },
         {
             title: "Ecommerce Front-end",
-            image: "https://i.postimg.cc/W361r7cv/2.webp",
+            image: "https://i.postimg.cc/KzYMcH3V/ecom.jpg",
         },
         {
             title: "Dental Ads Graphic",
-            image: "https://i.postimg.cc/HLx2FJnb/8.png",
+            image: "https://i.postimg.cc/FKpS44z3/ads.jpg",
         },
     ];
 
     return (
-        <section className="bg-primary text-white py-4 lg:pb-20 xl:py-16 px-6">
-            <div className="container mx-auto">
-                <div className='flex items-center justify-between border-b border-gray-400 py-5 xl:py-20'>
+        <section className="bg-primary text-white py-4 lg:py-12 xl:py-16 px-4 lg:px-6">
+            <div className="container mx-auto max-w-8xl">
+                <div className="flex items-center justify-between border-b border-gray-400 py-4 lg:py-8 xl:py-12">
                     <div>
-                        <h2 className="text-3xl xl:text-[60pt] font-medium">
+                        <h2 className="text-3xl xl:text-[60pt] xl:text-6xl font-medium">
                             Check the Works
                         </h2>
                     </div>
                     <div>
-                        <a href="/works" className='text-gray-400 hover:underline hover:text-white hover:duration-500 text-sm xl:text-2xl'>See more</a>
+                        <a href="/works" className="text-gray-400 hover:underline hover:text-white transition-colors duration-300 text-sm lg:text-xl xl:text-2xl">
+                            See more
+                        </a>
                     </div>
                 </div>
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-6 mb-5 md:mb-20 lg:mb-16 xl:mb-16">
+                <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 lg:gap-8 xl:gap-12 mt-8 lg:mt-12">
                     {projects.map((project, index) => (
                         <div
                             key={index}
-                            className={`relative sm:${index % 2 !== 0 ? 'translate-y-16 md:translate-y-20 md:ml-auto' : ''}`}
+                            className={`relative ${index % 2 !== 0 ? 'sm:mt-16 lg:mt-24' : ''}`}
                         >
-                            <div className="bg-primary w-full max-w-[700px] h-[350px] sm:h-[400px] md:h-[450px] rounded-lg overflow-hidden mt-5 xl:mt-16 group">
+                            <div className="bg-primary w-full aspect-[4/3] rounded-lg overflow-hidden group">
                                 <img
                                     src={project.image}
                                     alt={project.title}
-                                    className="w-[full] h-full object-cover transform transition-transform duration-500 group-hover:scale-110"
+                                    className="w-full h-full object-cover transform transition-transform duration-500 group-hover:scale-110"
                                 />
                             </div>
-                            <h3 className="mt-4 text-start text-lg sm:text-xl font-medium">{project.title}</h3>
+                            <h3 className="mt-4 text-lg lg:text-xl font-medium">{project.title}</h3>
                         </div>
                     ))}
                 </div>
