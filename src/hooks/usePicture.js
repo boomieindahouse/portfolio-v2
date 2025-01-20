118625b9833d5043b21b507208adbf4abb846be6
boomieindahouse <prawitsanat@hotmail.com>
2025-01-20 13:40:34 +0700

    edit mockup

--- src/hooks/usePicture.js
+++ src/hooks/usePicture.js
@@ -1,16 +1,19 @@
 const usePicture = () => {
     const pictures = [
-        'https://i.postimg.cc/SR8VVws5/1.png',
-        'https://i.postimg.cc/h47CS7rr/2.png',
-        // 'https://i.postimg.cc/W328rr9k/picc.jpg',
-        'https://i.postimg.cc/MTgd7WWV/4.png',
-        // 'https://i.postimg.cc/Y00xD7B2/355393367-200712936272602-6289766086379571234-n.jpg',
-        'https://i.postimg.cc/SN6D9FHY/6.png',
-        'https://i.postimg.cc/sgKmnPjV/7.png',
-        // 'https://i.postimg.cc/59KgYX8X/354397081-788469556086765-3935830681538853378-n.jpg',
-        'https://i.postimg.cc/26Lx8P46/9.png',
-        'https://i.postimg.cc/g0TDpYLQ/10.png'
+        'https://i.postimg.cc/2SfVprLS/mock1.png',
+        'https://i.postimg.cc/N0sFMD4h/mock2.png',
+        'https://i.postimg.cc/dVmDwpGm/mock3.png',
+        'https://i.postimg.cc/jdfLkL51/mock4.png',
+        'https://i.postimg.cc/50G2sVM1/mock5.png',
+        'https://i.postimg.cc/FR1KbK8D/mock6.jpg',
+        'https://i.postimg.cc/W453JZWd/mock7.jpg',
+        'https://i.postimg.cc/ZndKgMTN/mock8.jpg',
+        'https://i.postimg.cc/BZXpY9Kz/mock9.jpg',
+        'https://i.postimg.cc/rz2FLNmP/mock10.jpg',
+        'https://i.postimg.cc/pV4Z3GQL/mock11.jpg',
+        'https://i.postimg.cc/9F9p2d1t/mock12.jpg'
     ];
+    
 
     const getPicture = (index) => {
         // Ensure index is within bounds
