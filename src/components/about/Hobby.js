89f96035fe6740cd37faf90e4ee7f74fb374b83a
boomieindahouse <prawitsanat@hotmail.com>
2025-01-21 11:49:02 +0700

    edit margin modal

--- src/components/about/Hobby.js
+++ src/components/about/Hobby.js
@@ -64,7 +64,7 @@ const HobbiesSection = () => {
             {/* Modal */}
             {isModalOpen && (
                 <div
-                    className="fixed inset-0 bg-black bg-opacity-70 flex items-center justify-center z-50"
+                    className="fixed inset-0 bg-black bg-opacity-70 flex items-center justify-center z-50 mx-4"
                     onClick={closeModal}
                 >
                     <div className="relative">
