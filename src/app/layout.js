b9f67c831e689a9c97b55a71fca36f902f071232
boomieindahouse <prawitsanat@hotmail.com>
2025-02-13 10:09:19 +0700

    add meta tag open graph

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 
 import { useState, useEffect } from "react";
-import { Albert_Sans } from "next/font/google"; 
+import { Albert_Sans } from "next/font/google";
 import "@/styles/globals.css";
 import Navbar from "@/components/layout/Navbar";
 import SmoothScroll from "./providers/smooth-scroll";
@@ -26,10 +26,38 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        {/* Favicon & PWA */}
         <link rel="manifest" href="/manifest.json" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="https://i.postimg.cc/Nj630DBT/logo1.png" />
         <meta name="theme-color" content="#000000" />
+
+        {/* SEO Meta Tags */}
+        <meta name="title" content="Portfolio | Boomieindahouse" />
+        <meta name="description" content="Welcome to my portfolio website showcasing my projects and skills." />
+        <meta name="keywords" content="frontend developer, frontend developer and ux/ui designer, react portfolio, next.js portfolio, portfolio, boomieindahouse, elboomie, prawitsanat prapananyosanan, ux/ui designer, pawitsanat, pawitsanat papananyosanan" />
+        <meta name="author" content="Boomieindahouse" />
+        <meta name="robots" content="index, follow" />
+
+        {/* Open Graph Meta Tags (Facebook & Instagram) */}
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Portfolio | Boomieindahouse" />
+        <meta property="og:description" content="Welcome to my portfolio website showcasing my projects and skills." />
+        <meta property="og:image" content="https://i.postimg.cc/xTjXwZY3/homepage.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:image:alt" content="Portfolio Logo" />
+        <meta property="og:url" content="https://https://elboomie.vercel.app/" />
+        <meta property="og:site_name" content="Boomies Portfolio" />
+
+        {/* Twitter Card Meta Tags (X / Twitter) */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Portfolio | Your Name" />
+        <meta name="twitter:description" content="Welcome to my portfolio website showcasing my projects and skills." />
+        <meta name="twitter:image" content="https://i.postimg.cc/xTjXwZY3/homepage.png" />
+        <meta name="twitter:image:alt" content="Portfolio Logo" />
+        <meta name="twitter:site" content="@boomiebehumble" />
+        <meta name="twitter:creator" content="@boomiebehumble" />
       </head>
       <body className={`${albertSans.className} min-h-screen bg-black`}>
         {loading ? (
